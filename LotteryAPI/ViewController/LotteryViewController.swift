dcda2a891479f381bc5f43bb80e2c04b4c84cce9
onesunny2 <ippne996@gmail.com>
2025-01-14 17:04:41 +0900

    [design] 당첨번호안내, 날짜, 당첨결과 label 오토레이아웃 및 속성 설정

--- LotteryAPI/ViewController/LotteryViewController.swift
+++ LotteryAPI/ViewController/LotteryViewController.swift
@@ -17,6 +17,9 @@ class LotteryViewController: UIViewController {
     let bonusLabel = UILabel()
     var drawingNumsLabels: [UILabel] = []
     let lottoDrawPicker = UIPickerView()
+    let underlineUIView = UIView()
+    
+    var currentDraw: String = "913회"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,12 +38,14 @@ class LotteryViewController: UIViewController {
 }
 
 extension LotteryViewController: LotteryResult {
+    
     func configHierarchy() {
         view.addSubview(lottoDrawTextfield)
         view.addSubview(subtitleLabel)
         view.addSubview(lottoDateLabel)
         view.addSubview(resultLabel)
         view.addSubview(bonusLabel)
+        view.addSubview(underlineUIView)
 //        view.addSubview(lottoDrawPicker)
         
         for index in 0...6 {
@@ -54,6 +59,27 @@ extension LotteryViewController: LotteryResult {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
             make.height.equalTo(50)
         }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.top.equalTo(lottoDrawTextfield.snp.bottom).offset(30)
+        }
+        
+        lottoDateLabel.snp.makeConstraints { make in
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.centerY.equalTo(subtitleLabel)
+        }
+        
+        underlineUIView.snp.makeConstraints { make in
+            make.top.equalTo(subtitleLabel.snp.bottom).offset(10)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(2)
+        }
+        
+        resultLabel.snp.makeConstraints { make in
+            make.top.equalTo(underlineUIView.snp.bottom).offset(40)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+        }
     }
     
     func configView() {
@@ -66,7 +92,25 @@ extension LotteryViewController: LotteryResult {
         lottoDrawTextfield.placeholder = "조회를 원하는 회차를 골라주세요 :)"
         lottoDrawTextfield.textAlignment = .center
         
-        bonusLabel.text = "sdsddddd"
+        subtitleLabel.text = "당첨번호 안내"
+        subtitleLabel.font = .systemFont(ofSize: 17, weight: .medium)
+        subtitleLabel.textColor = .label
+        subtitleLabel.textAlignment = .left
+        
+        lottoDateLabel.text = "2020-06-30" + " 추첨"
+        lottoDateLabel.font = .systemFont(ofSize: 13, weight: .medium)
+        lottoDateLabel.textColor = .gray
+        lottoDateLabel.textAlignment = .right
+        
+        underlineUIView.backgroundColor = .systemGray6
+        
+
+        let title = currentDraw + " 당첨결과"
+        let attributedString = NSMutableAttributedString(string: title)
+        let stringLength = attributedString.length
+        attributedString.addAttributes([.foregroundColor: UIColor.orange, .font: UIFont.systemFont(ofSize: 30, weight: .semibold)],range: NSRange(location: 0, length: currentDraw.count))
+        attributedString.addAttributes([.foregroundColor: UIColor.label, .font: UIFont.systemFont(ofSize: 30, weight: .medium)], range: NSRange(location: currentDraw.count, length: stringLength - currentDraw.count))
+        resultLabel.attributedText = attributedString
     }
     
     
