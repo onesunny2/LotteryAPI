dbe291b023542ed07a229922ffb167ee7d7576af
onesunny2 <ippne996@gmail.com>
2025-02-24 20:04:52 +0900

    [feat] onlyDate 구하는 Date Extension 구현 및 최근 회차 구하는 로직 ViewModel에 구현

--- LotteryAPI/ViewModel/LotteryViewModel.swift
+++ LotteryAPI/ViewModel/LotteryViewModel.swift
@@ -25,4 +25,22 @@ final class LotteryViewModel: BaseViewModel {
     func transform(input: Input) -> Output {
         return Output()
     }
+    
+    
+    // MARK: 가장 최근 회차 구하기
+    static func recentLottoCount() -> Int {
+        
+        let dateFormat = DateFormatter()
+        dateFormat.dateFormat = "yyyy-MM-dd"
+        
+        guard let first = dateFormat.date(from: "2002-12-07") else { return 0 }
+        let firstDate = first.onlyDate
+        let currentDate = Date.now.onlyDate
+     
+        let dateGap = Calendar.current.dateComponents([.day], from: firstDate, to: currentDate).day ?? 0
+        
+        let recentDraw = (dateGap / 7) + 1
+        
+        return recentDraw
+    }
 }
